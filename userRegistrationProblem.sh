bd7d507bed71459ee0116d5304d8f7c4995ad901
Rachana R <rachanarrachu28@gmail.com>
2020-09-16 00:36:07 +0530

    Added : User's valid email-id  added

--- userRegistrationProblem.sh
+++ userRegistrationProblem.sh
@@ -1,8 +1,8 @@
 #! /bin/bash -x
 echo "Welcome to user registration program"
-read -p "Enter the lastName " lname
-pattern="^[A-Z]+[a-z]{3,}"
-if [[ $lname =~ $pattern ]]
+read -p "Enter the Users valid email " email
+pattern="^[a-z]+[0-9]*([-_+.][0-9a-z]+)*@[0-9a-z]+[.][a-z]{2,4}([,]{1})*([.][a-z]{2})*$"
+if [[ $email =~ $pattern ]]
 then
 echo valid
 else
