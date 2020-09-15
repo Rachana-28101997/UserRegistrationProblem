335f0a3da6b687074f4594335d9191fa4d4b20bd
Rachana R <rachanarrachu28@gmail.com>
2020-09-16 00:52:35 +0530

    Added : User's password with all condition checked and added

--- userRegistrationProblem.sh
+++ userRegistrationProblem.sh
@@ -1,5 +1,38 @@
 #! /bin/bash -x
 echo "Welcome to user registration program"
+
+#First name
+read -p "Enter the firstName " fname
+pattern="^[A-Z]+[a-z]{3,}"
+if [[ $fname =~ $pattern ]]
+then
+echo valid
+else
+echo invalid
+fi
+
+#Last name
+read -p "Enter the last name " Lname
+pattern="^[A-Z]+[a-z]{3,}"
+if [[ $Lname =~ $pattern ]]
+then
+echo valid
+else
+echo invalid
+fi
+
+#Email
+
+read -p "Enter the Users valid email " email
+pattern="^[a-z]+[0-9]*([-_+.][0-9a-z]+)*@[0-9a-z]+[.][a-z]{2,4}([,]{1})*([.][a-z]{2})*$"
+if [[ $email =~ $pattern ]]
+then
+echo valid
+else
+echo invalid
+fi
+
+#Mobile number
 read -p "Enter the Users mobile number " mobNum
 pattern="^[[+][1?9?]{2}]?[[:space:]]?[[0-9]{10}]*$"
 if [[ $mobNum =~ $pattern ]]
@@ -7,4 +40,13 @@ then
 echo "valid phone number"
 else
 echo "invalid phone number"
+fi
+
+#Password
+read -p "Enter the Users password " password
+if [[ ${#password} -ge 8 && $password =~ [[:upper:]] && $password=~ [[:lower:]]$password =~ [[:digit:]] && $password =~ 1 ]]
+then
+echo "valid password"
+else
+echo "invalid password"
 fi
