cbaad606a6d49d9b77981ed325639f293d0969b1
Rachana R <rachanarrachu28@gmail.com>
2020-09-16 00:29:14 +0530

    Added : User's valid first name added

--- userRegistrationProblem.sh
+++ userRegistrationProblem.sh
@@ -1,3 +1,10 @@
 #! /bin/bash -x
 echo "Welcome to user registration program"
-
+read -p "Enter the firstName " fname
+pattern="^[A-Z]+[a-z]{3,}"
+if [[ $fname =~ $pattern ]]
+then
+echo valid
+else
+echo invalid
+fi
